cf3885f38fa1bdc86a03753378306906742beece
Tomotaka Ito <tomotaka.ito@gmail.com>
2017-08-08 16:07:33 +0900

    specify packages to install

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='daynextprev',
-    version='0.0.2',
+    version='0.0.3',
     description='calculate next/prev day from y/m/d input',
     author='Tomotaka Ito',
     author_email='tomotaka.ito@gmail.com',
     url='https://github.com/tomotaka/day-nextprev',
-    packages=find_packages(),
+    packages=['daynextprev'] + find_packages(),
     license=open('LICENSE').read(),
     include_package_data=True,
     install_requires=[
